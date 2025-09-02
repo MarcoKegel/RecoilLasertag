a1d2e158ce2d4932d27ccb284ac18b013719c14b
Marco Meixner <info@meixners.org>
2025-09-02 20:36:12 +0200

    string converter helper

--- BluetoothClient/Esp32BluetoothTest/src/StringConverter.cpp
+++ BluetoothClient/Esp32BluetoothTest/src/StringConverter.cpp
@@ -12,7 +12,20 @@ std::string StringConverter::stringToHex(const std::string &input)
 
   return hexStream.str();
 }
+std::string StringConverter::stringToHex(const char &c)
+{
+  std::ostringstream hexStream;
+  hexStream << std::hex << std::setfill('0'); // Ensures leading zeros if needed
+  hexStream << std::setw(2) << static_cast<int>(c); // Converts each char to hex
+  return hexStream.str();
+}
 
+std::string StringConverter::stringToBinary(const char &c)
+{
+  std::ostringstream binaryStream;
+    binaryStream << std::bitset<8>(c); // Convert each char to 8-bit binary
+  return binaryStream.str();
+}
 std::string StringConverter::stringToBinary(const std::string &input)
 {
   std::ostringstream binaryStream;
